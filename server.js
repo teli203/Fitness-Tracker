c435f2cfbe1491da6d484f1247a807c9b03d9025
Telicia Watson <teli203@aol.com>
2021-01-20 23:18:02 -0600

    port variable

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 17943;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
